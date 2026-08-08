13880b8214425187030b32f3b8d1ce554e995a15
srtr <srtr77@gmail.com>
2026-08-08 18:44:42 +0300

    fix: correct api routes in message.ts and chat.ts

--- src/api/message.ts
+++ src/api/message.ts
@@ -2,17 +2,17 @@ import { api } from './client';
 
 
 export async function createMessage(chatId: string, content: string) {
-    const res = await api.post('/message/create', {chatId, content });
+    const res = await api.post('/messages/make', {chatId, content });
     return res.data;
 }
 
 export async function findAllInChat(chatId: number) {
-    const res = await api.post('/message/create', { chatId});
+    const res = await api.get(`/messages/chat/${chatId}`);
     return res.data;
 }
 
 export async function deleteMessage(messageId: number) {
-    const res = await api.post('/message/create', { messageId});
+    const res = await api.post(`/messages/${messageId}/delete`);
     return res.data;
 }
 
