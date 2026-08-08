1b7a513dbe4cdcafbd0122fbc1287c97489b86fe
srtr <srtr77@gmail.com>
2026-08-08 18:15:44 +0300

    refactor: update messages to remove chatId parameter

--- src/api/message.ts
+++ src/api/message.ts
@@ -1,18 +1,18 @@
 import { api } from './client';
 
 
-export async function createMessage(senderId: number, chatId: string, content: string) {
-    const res = await api.post('/message/create', { senderId, chatId, content });
+export async function createMessage(chatId: string, content: string) {
+    const res = await api.post('/message/create', {chatId, content });
     return res.data;
 }
 
-export async function findAllInChat(chatId: number, userId: number) {
-    const res = await api.post('/message/create', { chatId, userId });
+export async function findAllInChat(chatId: number) {
+    const res = await api.post('/message/create', { chatId});
     return res.data;
 }
 
-export async function deleteMessage(messageId: number, userId: number) {
-    const res = await api.post('/message/create', { messageId, userId });
+export async function deleteMessage(messageId: number) {
+    const res = await api.post('/message/create', { messageId});
     return res.data;
 }
 
