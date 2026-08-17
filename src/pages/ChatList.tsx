ead73d6531a480dcd983be1b77d4a0fe1f4fb742
srtr <srtr77@gmail.com>
2026-08-17 11:05:08 +0400

    refactor: erased and exported duplicate type code and get info functions

--- src/pages/ChatList.tsx
+++ src/pages/ChatList.tsx
@@ -3,28 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import { List, ListItemButton, ListItemText, Avatar, Box, Typography, Button, CircularProgress } from '@mui/material';
 import { getChats } from '../api/chats';
 import { useAuth } from '../context/AuthContext';
-import { avatarUrl } from '../api/user';
-
-interface UserProfile {
-  id: number;
-  username: string;
-  avatarUrl?: string | null;
-}
-
-interface ChatMember {
-  id: number;
-  userId: number;
-  user?: UserProfile;
-}
-
-interface Chat {
-  id: number;
-  name: string | null;
-  isGroup: boolean;
-  imageUrl?: string;
-  members: ChatMember[];
-}
-
+import {getChatDisplayInfo} from '../util/chatDisplay';
+import type {Chat} from '../types/types';
 
 export default function ChatList() {
   const [chats, setChats] = useState<Chat[]>([]);
@@ -39,30 +19,6 @@ export default function ChatList() {
   }, []);
 
 
-  const getChatDetails = (chat: Chat) => {
-    if (chat.isGroup) {
-      return {
-        title: chat.name || `Group Chat #${chat.id}`,
-        subtitle: `${chat.members.length} members`,
-        avatarUrl: avatarUrl(chat.imageUrl ?? null),
-      };
-    }
-    const otherMember = chat.members.find((m) => m.userId !== user?.id);
-
-    if (!otherMember) {
-      const selfMember = chat.members.find((m) => m.userId === user?.id);
-      const selfUsername = selfMember?.user?.username || user?.username || 'You';
-      return {
-        title: `${selfUsername} (You)`,
-        avatarUrl: avatarUrl(selfMember?.user?.avatarUrl ?? null) ,
-      };
-    }
-    return {
-      title: otherMember.user?.username || 'Direct Message',
-      subtitle: 'Direct Message',
-      avatarUrl: avatarUrl(otherMember.user?.avatarUrl ?? null),
-    };
-  };
 
   if (loading) return <CircularProgress sx={{ mt: 4, mx: 'auto', display: 'block' }} />;
 
@@ -78,12 +34,12 @@ export default function ChatList() {
 
       <List>
         {chats.map((chat) => {
-          const { title, subtitle, avatarUrl } = getChatDetails(chat);
+          const { title, subtitle, src } = getChatDisplayInfo(chat, user?.id );
 
           return (
             <ListItemButton key={chat.id} onClick={() => navigate(`/chats/${chat.id}`)}>
-              <Avatar src={avatarUrl || undefined} sx={{ mr: 2 }}>
-                {!avatarUrl && title[0]?.toUpperCase()}
+              <Avatar src={src || undefined} sx={{ mr: 2 }}>
+                {!src && title[0]?.toUpperCase()}
               </Avatar>
               <ListItemText primary={title} secondary={subtitle} />
             </ListItemButton>
